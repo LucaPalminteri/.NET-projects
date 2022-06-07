8e24c2ba0b7ec12a18b332002f13538e26ee4ef0
LucaPalminteri <lucapalminteri02@gmail.com>
2022-06-07 20:28:30 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// First mini project in .NET and a little bit of C#
+
+Console.WriteLine("Insert name: ");
+var name = Console.ReadLine();
+Console.WriteLine("Insert age: ");
+var age = Console.ReadLine();
+Console.WriteLine($"Your name: {name}\nYour age: {age}");
